34d0f27dee17c2f411268bb37e97619646fdba47
Michael Zanetti <michael_zanetti@gmx.net>
2019-04-25 13:08:51 +0200

    Update licensing

--- libnymea-mqtt/mqttpacket.h
+++ libnymea-mqtt/mqttpacket.h
@@ -1,22 +1,29 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                                                                         *
- *  Copyright (C) 2018 Michael Zanetti <michael.zanetti@guh.io>            *
- *                                                                         *
- *  This file is part of nymea.                                            *
- *                                                                         *
- *  nymea is free software: you can redistribute it and/or modify          *
- *  it under the terms of the GNU General Public License as published by   *
- *  the Free Software Foundation, version 2 of the License.                *
- *                                                                         *
- *  nymea is distributed in the hope that it will be useful,               *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *  GNU General Public License for more details.                           *
- *                                                                         *
- *  You should have received a copy of the GNU General Public License      *
- *  along with nymea. If not, see <http://www.gnu.org/licenses/>.          *
- *                                                                         *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/****************************************************************************
+**
+** Copyright (C) 2019 guh GmbH
+** Contact: https://www.nymea.io/licensing/
+**
+** This file is part of nymea
+**
+** Commercial License Usage
+** Licensees holding valid commercial nymea licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and guh GmbH. For licensing terms
+** and conditions see https://www.nymea.io/terms-conditions. For further
+** information use the contact form at https://www.nymea.io/imprint.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv3
+** included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html
+**
+**
+****************************************************************************/
+
 
 #ifndef MQTTPACKET_H
 #define MQTTPACKET_H
