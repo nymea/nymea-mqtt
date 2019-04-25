a3d41b6cfc9df3f2d4462a2f2f390f36548521ac
Michael Zanetti <michael_zanetti@gmx.net>
2019-04-25 13:50:57 +0200

    fix header for GPLv3

--- server/main.cpp
+++ server/main.cpp
@@ -16,7 +16,7 @@
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
 ** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
+** Foundation and appearing in the file LICENSE.GPLv3 included in the
 ** packaging of this file. Please review the following information to
 ** ensure the GNU General Public License version 3.0 requirements will be
 ** met: https://www.gnu.org/licenses/gpl.html
