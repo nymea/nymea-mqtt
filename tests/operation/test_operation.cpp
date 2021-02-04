718a495437f966ffcc4cf5aca187190ceab1983e
Timon <timon.baetz@moia.io>
2021-02-04 13:19:51 +0100

    Fix MqttPackdt payload assignment.
    QByteArray(str) assumes that str is '\0'-terminated
    which is not the case when sending binary data.

--- tests/operation/test_operation.cpp
+++ tests/operation/test_operation.cpp
@@ -82,6 +82,8 @@ private slots:
 
     void testEmptyClientId();
 
+    void testBinaryPaylaod();
+
 private:
     // Connects and waits for the MQTT CONNECT to be finished
     MqttClient *connectAndWait(const QString &clientId, bool cleanSession = true, quint16 keepAlive = 300, const QString &willTopic = QString(), const QString &willMessage = QString(), Mqtt::QoS willQoS = Mqtt::QoS0, bool willRetain = false);
@@ -818,6 +820,19 @@ void OperationTests::testEmptyClientId()
     QTRY_VERIFY2(client3.second->count() == 1, "Client did not emit connected signal");
     QTRY_COMPARE(client3.second->first().at(0).value<Mqtt::ConnectReturnCode>(), Mqtt::ConnectReturnCodeIdentifierRejected);
     QTRY_VERIFY2(client3.first->isConnected() == false, "Connection should have been dropped");
+}
+
+void OperationTests::testBinaryPaylaod()
+{
+    MqttClient *client = connectAndWait("");
+    QVERIFY2(client->isConnected(), "Client did not connect");
+    client->subscribe("#");
+    const char binData[] = {'\xA5', '\x20', '\x00', '\x04', '\x00', '\x52'};
+    QByteArray payload(QByteArray::fromRawData(binData, 6));
+    QSignalSpy publishReceivedSpy(client, &MqttClient::publishReceived);
+    client->publish("testtopic", payload);
+    QTRY_VERIFY2(publishReceivedSpy.count() == 1, "Did not receive publish message");
+    QCOMPARE(publishReceivedSpy.first().at(1).toByteArray(), payload);
 }
 
 #endif
